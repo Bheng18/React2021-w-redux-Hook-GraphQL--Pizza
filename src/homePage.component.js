50e2f16a6ca69c98eb0a94835beb5697cf8ca793
Bheng18 <joebert.fundador@gmail.com>
2021-01-09 19:32:05 -0800

    created homepge nd first commit

---
+++ src/homePage.component.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./homePage.styles.scss";
+
+const HomePage = () => (
+    <div className='homepage'>
+       <div className='directory-menu'>
+          <div className='menu-item'>
+             <div className='content'>
+               <h1 className='title'>CJ</h1>
+                 <span className='subtitle'>Tim</span>
+             </div>
+          </div>
+
+       <div className='menu-item'>
+             <div className='content'>
+               <h1 className='title'>Cyben</h1>
+                 <span className='subtitle'>Fun</span>
+             </div>
+          </div>
+
+          <div className='menu-item'>
+             <div className='content'>
+               <h1 className='title'>Fith</h1>
+                 <span className='subtitle'>JenS</span>
+             </div>
+          </div>
+
+          <div className='menu-item'>
+             <div className='content'>
+               <h1 className='title'>Cyben</h1>
+                 <span className='subtitle'>Fun</span>
+             </div>
+          </div>
+          
+          <div className='menu-item'>
+             <div className='content'>
+               <h1 className='title'>Fith</h1>
+                 <span className='subtitle'>JenS</span>
+             </div>
+          </div>
+
+
+       </div>
+    </div>
+);
+
+export default HomePage;
