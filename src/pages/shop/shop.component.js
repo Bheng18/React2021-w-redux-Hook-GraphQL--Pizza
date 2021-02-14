6544afc358fa99a35a1005796d60db2ba283fcd6
Bheng18 <joebert.fundador@gmail.com>
2021-02-14 00:06:57 -0800

    optimized directory page and shop page

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,29 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import PreviewCollection from "../../components/preview-collections/preview-collection.component";
-import SHOP_DATA from "./shop.data";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <PreviewCollection key={id} {...otherCollectionProps} />
+            ))
         }
-    }
+    </div>
+);
 
-    render(){
-        const {collections} = this.state;
-        return(
-          <div className='shop-page'>
-             {
-                 collections.map(({id, ...otherCollectionProps}) => (
-                     <PreviewCollection key={id} {...otherCollectionProps} />
-                 ))
-             }
-          </div>
-        );
-    }
-
-}
-
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+})
+   
+export default connect(mapStateToProps)(ShopPage);
